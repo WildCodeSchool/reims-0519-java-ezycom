4075e2d7e8cb9b23966f894c53c4f8e4fe5317f9
BryanNun <nuninger.bryan@gmail.com>
2019-08-07 09:22:42 +0200

    Unnecessary file deletion

--- src/main/java/com/ezycom/projectEzycom/repositories/RemunerationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ezycom.projectEzycom.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ezycom.projectEzycom.entities.Remuneration;
-
-@Repository
-public interface RemunerationRepository extends JpaRepository<Remuneration, Long> {
-}
--- src/main/java/com/ezycom/projectEzycom/repositories/VendreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ezycom.projectEzycom.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ezycom.projectEzycom.entities.Vendre;
-
-@Repository
-public interface VendreRepository extends JpaRepository<Vendre, Long> {
-}
