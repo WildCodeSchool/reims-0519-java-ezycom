b768ac502bf016ebd5fa284a94eb34f87cb84554
Lefevre Anthony <lefevre.anthony.dev@gmail.com>
2019-08-12 12:02:26 +0200

    mapping login

--- src/main/java/com/ezycom/projectEzycom/repositories/CommercialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ezycom.projectEzycom.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ezycom.projectEzycom.entities.Commercial;
-
-@Repository
-public interface CommercialRepository extends JpaRepository<Commercial, Long> {
-}
