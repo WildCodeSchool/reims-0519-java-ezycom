d73a52b046922b7c7f4a6fc261ae92ce1d5d0918
BryanNun <nuninger.bryan@gmail.com>
2019-08-12 09:43:02 +0200

    Change Entities Commercial

---
+++ src/main/java/com/ezycom/projectEzycom/entities/User.java
@@ -0,0 +1,113 @@
+package com.ezycom.projectEzycom.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User{
+    public User(){
+    }
+
+    public User(String email, String password, String role){
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+
+    
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getemail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getpassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setpassword(String password) {
+        this.password = password;
+    }
+
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return String return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
