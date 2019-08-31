fe408b04c6a62b68794eb12254ee1ef9482a9292
BryanNun <nuninger.bryan@gmail.com>
2019-09-01 00:26:19 +0200

    Added Html and controller for create users

--- src/main/java/com/ezycom/projectEzycom/controllers/UserController.java
+++ src/main/java/com/ezycom/projectEzycom/controllers/UserController.java
@@ -2,13 +2,21 @@ package com.ezycom.projectEzycom.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.ezycom.projectEzycom.entities.User;
 import com.ezycom.projectEzycom.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -17,20 +25,32 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/users/browse")
+    public String browse(Model model) {
+        return "/users/browse";
+    }
+
     @GetMapping("/users/create")
-    public String create(Model model) {
-        List<User> users = userRepository.findAll();
-        model.addAttribute("users", users);
-        return "users/create";
-
+    public String create(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        return "/users/create";
     }
 
-    @GetMapping("/users/associate")
-    public String associate(Model model) {
-        List<User> users = userRepository.findAll();
-        model.addAttribute("users", users);
-        return "users/associate";
+    @PostMapping("/users")
+    public String validate(@Valid User user, BindingResult bindingResult, RedirectAttributes redirectAttrs) {
+        if(bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
 
-    }
+            }
+            redirectAttrs.addFlashAttribute("user", user);
+            return "redirect:/users/create";
+        }
+         else {
+            userRepository.save(user);
+            return "redirect:/users/create";
+        }
+​
+​
+     }
 
 }
