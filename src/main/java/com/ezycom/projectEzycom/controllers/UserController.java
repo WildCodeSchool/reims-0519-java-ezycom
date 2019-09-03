8a1732f990b83c7cbdf1cac82098fa14f457f9e3
BryanNun <nuninger.bryan@gmail.com>
2019-09-03 17:51:22 +0200

    Added import

--- src/main/java/com/ezycom/projectEzycom/controllers/UserController.java
+++ src/main/java/com/ezycom/projectEzycom/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.ezycom.projectEzycom.controllers;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import com.ezycom.projectEzycom.entities.User;
@@ -15,7 +13,6 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
