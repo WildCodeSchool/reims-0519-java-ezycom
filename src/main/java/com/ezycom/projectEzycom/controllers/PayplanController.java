e0fcaa60e8d1d37d3e21c1af5ba3f23d455dafe6
Wilder <n.huraux@hotmail.com>
2019-09-05 14:31:18 +0200

    changement pas terminé

--- src/main/java/com/ezycom/projectEzycom/controllers/PayplanController.java
+++ src/main/java/com/ezycom/projectEzycom/controllers/PayplanController.java
@@ -1,6 +1,7 @@
 package com.ezycom.projectEzycom.controllers;
 
 import javax.validation.Valid;
+import java.util.List;
 
 import com.ezycom.projectEzycom.entities.Payplan;
 import com.ezycom.projectEzycom.repositories.PayplanRepository;
@@ -12,7 +13,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -23,8 +28,22 @@ public class PayplanController {
 
     @GetMapping("/payplans/browse")
     public String browse(Model model) {
-        return "/payplans/browse";
+       
+    
+
+    List<Payplan> payplans = payplanRepository.findAll();
+    model.addAttribute("payplans", payplans);
+    return "payplans/browse";
+
     }
+    @RequestMapping(value="/payplans/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String delete(@PathVariable String id) {
+    studentService.delete(id);
+    return "Successfully deleted";
+}
+
+
 
     @GetMapping("/payplans/create")
     public String create(@ModelAttribute Payplan payplan, Model model) {
