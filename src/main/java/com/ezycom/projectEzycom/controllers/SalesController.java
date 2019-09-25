1148891064acb87fdebfbd40316252aa82f53877
Carole Chevalier <ateliercakocako@gmail.com>
2019-09-25 14:27:38 +0200

    added commission entity

--- src/main/java/com/ezycom/projectEzycom/controllers/SalesController.java
+++ src/main/java/com/ezycom/projectEzycom/controllers/SalesController.java
@@ -2,6 +2,13 @@ package com.ezycom.projectEzycom.controllers;
 
 import java.util.List;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.ezycom.projectEzycom.entities.Commission;
 import com.ezycom.projectEzycom.entities.Sales;
 import com.ezycom.projectEzycom.repositories.SalesRepository;
 
@@ -15,11 +22,16 @@ public class SalesController {
 
     @Autowired
     private SalesRepository salesRepo;
-
+    
     @GetMapping("/sales")
     public String browse(Model model) {
+
         List<Sales> sales = salesRepo.findAll();
         model.addAttribute("sales", sales);
+    
+        List<Commission> commissions = salesRepo.findByCommercial();
+        model.addAttribute("commissions", commissions);
+
         return "import/sales";
     }
 }
