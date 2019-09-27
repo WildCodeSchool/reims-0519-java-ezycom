80aa005689e0e3ed972744ad9e847eddb62b7790
BryanNun <nuninger.bryan@gmail.com>
2019-09-27 18:22:52 +0200

    filter

--- src/main/resources/static/filter_sales.js
+++ src/main/resources/static/filter_sales.js
@@ -1,35 +1,33 @@
-$("#sale").on("change", function() {
-    let sale = $(this).val(); 
-    $("#month").find("option").hide();
-    $("#month").val('Sélectionnez...');
-    $("#commercial").find("option").hide();
-    $("#commercial").val('Sélection du commercial');
-    if (sale) {
-        $("#month").find("option[filter='" + sale + "']")
-                .show();
-
-        let optionValues =[];
-        $("#month option").each(function(){
-            if (this.getAttribute('filter') == $("#sale").val()) {
-                if($.inArray(this.value, optionValues) >-1){
-                    $(this).hide();
-                }
-                else {
-                    optionValues.push(this.value);
-                }
+let optionValues =[];
+$("#sale option").each(function(){
+    if($.inArray(this.getAttribute('commercial') , optionValues) >-1){
+        $(this).hide();
+    }
+    else {
+        optionValues.push(this.getAttribute('commercial'));
+    }
+});
+​
+function updateTable() {
+    filter("commercial_list", 0);
+    filter("sales_list", 3);
+}
+​
+function filter(tableId, commercialColumnNumber) {
+    let myFilterSelect = document.getElementById("sale");
+    let commercial = myFilterSelect.options[myFilterSelect.selectedIndex].text;
+    let table = document.getElementById(tableId);
+    let tr = table.getElementsByTagName("tr");
+    let td, i, txtValue;
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[commercialColumnNumber];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue == commercial) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
             }
-        });
+        }       
     }
-});
-
-$("#month").on("change", function() {
-    let sale = $("#sale").val();
-    let month = $(this).val().toLowerCase();
-    $("#commercial").find("option").hide();
-    $("#commercial").val('Sélectionnez...');
-    if (month) {
-        $("#commercial").find("option[filter='" + sale + " " + month + "']")
-                    .show()
-                    .prop('selected', true);
-    }
-});
+}
