066a2da97abf72d59020edfba6ce238d0249df88
BryanNun <nuninger.bryan@gmail.com>
2019-10-01 09:11:34 +0200

    added second function js

--- src/main/resources/static/filter_sales.js
+++ src/main/resources/static/filter_sales.js
@@ -7,7 +7,7 @@ $("#sale option").each(function(){
         optionValues.push(this.getAttribute('commercial'));
     }
 });
-
+optionValues =[];
 $("#month option").each(function(){
     if($.inArray(this.getAttribute('month') , optionValues) >-1){
         $(this).hide();
@@ -17,12 +17,13 @@ $("#month option").each(function(){
     }
 });
 
-function updateTable() {
-    filter("commercial_list", 0);
-    filter("sales_list", 3);
+function updateTableCommercial() {
+    filterCommercial("commercial_list", 0);
+    filterCommercial("sales_list", 3);
 }
-function filter(tableId, commercialColumnNumber) {
-    let myFilterSelect = document.getElementById("sale");
+function filterCommercial(tableId, commercialColumnNumber) {
+    console.log('here');
+    let myFilterSelect = document.getElementById('commercial');
     let commercial = myFilterSelect.options[myFilterSelect.selectedIndex].text;
     let table = document.getElementById(tableId);
     let tr = table.getElementsByTagName("tr");
@@ -32,6 +33,28 @@ function filter(tableId, commercialColumnNumber) {
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue == commercial) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
+
+function updateTableMonth() {
+    filterMonth("sales_list", 2);
+}
+function filterMonth(tableId, monthColumnNumber) {
+    let myFilterSelect = document.getElementById('month');
+    let month = myFilterSelect.options[myFilterSelect.selectedIndex].text;
+    let table = document.getElementById(tableId);
+    let tr = table.getElementsByTagName("tr");
+    let td, i, txtValue;
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[monthColumnNumber];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue == month) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
