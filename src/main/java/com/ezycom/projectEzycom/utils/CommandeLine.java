af1abee888de799a8c311cc46b9e0a4cd7f409c5
Wilder <n.huraux@hotmail.com>
2019-10-01 17:12:36 +0200

    corecction erreur

--- src/main/java/com/ezycom/projectEzycom/utils/CommandeLine.java
+++ src/main/java/com/ezycom/projectEzycom/utils/CommandeLine.java
@@ -112,7 +112,6 @@ import org.springframework.stereotype.Component;
                     
                     201906006L, //numero de commande
                     "08.07.2019", // date de commande
-                    "07.10.2019", //date de saisie ADV
                     "Richard Jacques", // Commercial
                     "S0003", //code produit
                     80, //quantité
@@ -126,7 +125,6 @@ import org.springframework.stereotype.Component;
                     
                     2019060008L, //numero de commande
                     "08.07.2019", // date de commande
-                    "07.10.2019", //date de saisie ADV
                     "Richard Jacques", // Commercial
                     "S0001", //code produit
                     600, //quantité
@@ -140,7 +138,6 @@ import org.springframework.stereotype.Component;
                     
                     2019060010L, //numero de commande
                     "08.07.2019", // date de commande
-                    "07.10.2019", //date de saisie ADV
                     "Richard Jacques", // Commercial
                     "S0001", //code produit
                     10, //quantité
