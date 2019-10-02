79ccd938bfc3c9700fb676f18181063e7977c32f
Wilder <n.huraux@hotmail.com>
2019-10-02 17:54:07 +0200

    ajout du controllerComercial et ajout du filtre par commercial

--- src/main/java/com/ezycom/projectEzycom/controllers/HomeController.java
+++ src/main/java/com/ezycom/projectEzycom/controllers/HomeController.java
@@ -5,34 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.ezycom.projectEzycom.entities.Commission;
-import com.ezycom.projectEzycom.entities.Sale;
 import com.ezycom.projectEzycom.entities.User;
-import com.ezycom.projectEzycom.repositories.SaleRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private SaleRepository saleRepo;
-
     @GetMapping("/home")
     public String forwardByRole(Model model, Authentication authentication, @RequestParam(required = false) String month) {
         User currentUser = (User) authentication.getPrincipal();
         if (currentUser.getRole().equals("Commercial")) {
-            List<Sale> listSales = saleRepo.findAll();
-            model.addAttribute("listSales", listSales);
-            List<Sale> commissions = saleRepo.findAllSalesByCommercialAndByMonth(month, currentUser.getFullName());
-            List<Sale> sales = saleRepo.findAllSalesByCommercial(currentUser.getFullName());
-            model.addAttribute("commissions", commissions);
-            model.addAttribute("sales", sales);
-            return "/commercials/read";
+            return "forward:/commercial";
         } else {
             return "forward:/sales";
         }
