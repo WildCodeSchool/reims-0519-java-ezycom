f0b79822098fdd4288bbf0f1f8f4e7f541c358e4
Carole Chevalier <ateliercakocako@gmail.com>
2019-10-03 17:31:33 +0200

    test historic

--- src/main/java/com/ezycom/projectEzycom/entities/PayplanUser.java
+++ src/main/java/com/ezycom/projectEzycom/entities/PayplanUser.java
@@ -1,7 +1,6 @@
 package com.ezycom.projectEzycom.entities;
 
 import java.sql.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
